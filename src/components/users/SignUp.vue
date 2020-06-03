c9a9afb45bb06db74305a359c2f722252130e591
Arthur <arthurquique@protonmail.com>
2020-06-03 16:04:55 +0200

    Commit chez moi

--- src/components/users/SignUp.vue
+++ src/components/users/SignUp.vue
@@ -1,82 +1,109 @@
 <template>
-    
- 
-        <v-dialog v-model="dialog" persistent max-width="600px">
+  <v-dialog v-model="dialog" persistent max-width="600px">
+    <!-- Bouton d'affichage de la popup de création de compte -->
+    <template v-slot:activator="{on}">
+      <v-btn v-on="on" class="white--text" color="#6B4EE0">Créer un compte</v-btn>
+    </template>
 
-        <!-- Bouton d'affichage de la popup de création de compte -->
-        <template v-slot:activator="{on}">
-            <v-btn v-on="on" class="white--text" color="#6B4EE0"> Créer un compte </v-btn>
-        </template>
+    <!-- Toolbar de la popup de connexion -->
+    <v-card>
+      <v-toolbar color="#6B4EE0" dark flat>
+        <v-toolbar-title>Créer un compte</v-toolbar-title>
+        <v-spacer></v-spacer>
+      </v-toolbar>
 
-          <!-- Toolbar de la popup de connexion -->
-          <v-card>
-            <v-toolbar color="#6B4EE0" dark flat >
-              <v-toolbar-title>Créer un compte</v-toolbar-title>
-              <v-spacer></v-spacer>
-            </v-toolbar>
+      <!-- Champs de l'id et du mot de passe -->
+      <v-card-text>
+        <v-form>
+          <v-text-field
+            label="Nom de joueur*"
+            name="login"
+            id="login"
+            v-model="login"
+            prepend-icon="mdi-account"
+            color="#6B4EE0"
+            type="text"
+            required
+          ></v-text-field>
 
-            <!-- Champs de l'id et du mot de passe -->
-            <v-card-text>
-              <v-form>
-                <v-text-field
-                  label="Veuillez entrer un nom de joueur*"
-                  name="login"
-                  prepend-icon="mdi-account"
-                  color="#6B4EE0"
-                  type="text"
-                  required></v-text-field>
+          <v-text-field
+            label="Adresse mail*"
+            name="email"
+            prepend-icon="mdi-mail"
+            id="email"
+            v-model="email"
+            type="email"
+            required
+          ></v-text-field>
 
-                  <v-text-field
-                  id="password"
-                  label="Veuillez entrer votre mot de passe*"
-                  name="password"
-                  prepend-icon="mdi-lock"
-                  color="#6B4EE0"
-                  type="password"
-                  required></v-text-field>
+          <v-text-field
+            id="password"
+            label="Mot de passe*"
+            name="password"
+            prepend-icon="mdi-lock"
+            color="#6B4EE0"
+            v-model="password"
+            type="password"
+            required
+          ></v-text-field>
 
-                <v-text-field
-                  id="password"
-                  label="Veuillez confirmer votre mot de passe*"
-                  name="passwordcheck"
-                  prepend-icon="mdi-lock"
-                  color="#6B4EE0"
-                  type="password"
-                  required></v-text-field>
+          <v-text-field
+            id="confirmPassword"
+            label="Confirmez votre mot de passe*"
+            name="confirmPassword"
+            prepend-icon="mdi-lock"
+            color="#6B4EE0"
+            v-model="confirmPassword"
+            type="password"
+            :rules="[comparePasswords]"
+          ></v-text-field>
 
-                </v-form>
-              </v-card-text>
-
-              <!-- Boutons de création d'un compte et de connexion -->
-              <v-card-actions justify>
-                <v-btn class="white--text" color="#6B4EE0" @click="dialog = false"> Fermer </v-btn>
-                <v-spacer></v-spacer>
-                <v-btn class="white--text" color="#6B4EE0" > Créer un compte</v-btn>
-              </v-card-actions>
-
-          </v-card>
-        </v-dialog>
-  
-    
+          <!-- Boutons de création d'un compte et de connexion -->
+          <v-card-actions justify>
+            <v-btn class="white--text" color="#6B4EE0" @click="dialog = false">Fermer</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn type="submit" class="white--text" color="#6B4EE0">Créer un compte</v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
- export default {
-    data: () => ({
-      dialog: false,
-    }),
-    props: {
-      source: String,
-    },
-      methods: {
-
+export default {
+  data: () => ({
+    dialog: false,
+    login: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  }),
+  props: {
+    source: String
+  },
+  computed: {
+    comparePasswords() {
+      return this.password !== this.confirmPassword ? 'Les mots de passe ne correspondent pas' : true;
+    }
+  },
+  methods: {
+    onSignUp() {
+      // Vuex
+      console.log({
+        login: this.login,
+        email: this.email,
+        password: this.password,
+        confirmPassword: this.confirmPassword
+      });
     }
   }
+};
 </script>
 
 <style scoped>
-  .SignUp{
-    margin:0px;
-    padding:0px;
-  }
+.SignUp {
+  margin: 0px;
+  padding: 0px;
+}
 </style>
